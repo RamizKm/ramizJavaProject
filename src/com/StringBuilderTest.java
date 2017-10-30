8fcd119fc435ce00ddeaf60a0987ffa5d947c2f0
ramizkm <mominramiz@gmail.com>
2017-10-30 19:53:05 +0530

    git commit for strinbuffer.

---
+++ src/com/StringBuilderTest.java
@@ -0,0 +1,20 @@
+package com;
+
+public class StringBuilderTest {
+	
+	public static void main(String[] args) {
+		
+		StringBuilder sb=new StringBuilder();
+		sb.append("rkm\n");
+		sb.append("fkm");
+		sb.append("tkm");
+		sb.append("skm");
+		System.out.println(sb);
+		
+		
+	//	String buffer is synchrnozied.it is thread safe
+		
+		
+	}
+
+}
