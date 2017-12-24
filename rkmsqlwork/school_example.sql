1100fad138cf2e57a9b57a7c601550b1c9bc6a7f
ramizkm <mominramiz@gmail.com>
2017-12-24 14:29:53 +0530

    test file

--- rkmsqlwork/school_example.sql
+++ rkmsqlwork/school_example.sql
@@ -1,3 +1,6 @@
+--Oracle 11g Express Edition
+-- Put your Oracle SQL statement here and execute it
+
 select * from users;
 
 select * from school;
@@ -22,12 +25,32 @@ insert into rkmschool(rkmid,rkmschlname) values (1,'rkmschool');
 
 select * from rkmschool;
 
+drop table rkmstudent;
+
 create table rkmstudent (
-studentid varhar2(100),
-stdfname varchar2(100),
+studentid varhar2(100) not null,
+stdfname varchar2(100) not null,
 stdmname varchar2(100),
 stdlname varchar2(100),
-stddateofbirth varchar2(100));
+stddateofbirth varchar2(100) not null
+);
 
 
 alter table rkmstudent add constraint primary key (studentid,stdfname,stddateofbirth);
+
+insert into rkmstudent(studid,stdfname,stdmname) values ('rkm123','rkm fist','rkm first mname');
+
+describe table rkmstudent;
+
+drop table aulog;
+
+create table aulog (
+activitytype varchar2(100),
+activitiyid varchar2(100),
+activityvalue varchar2(100) 
+);
+
+alter table aulog add constraint primary key(activitytype,activitiyid,activityvalue);
+
+
+
