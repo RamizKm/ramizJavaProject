c7484def0ade0cc7cad4d01b9a4b1438b6a62a0f
ramizkm <mominramiz@gmail.com>
2017-12-24 14:09:30 +0530

    syst msg

--- OtherMainClass.java
+++ OtherMainClass.java
@@ -3,6 +3,12 @@ public class OtherMainClass {
 public static void main(String[] arg) {
 
 System.out.println("in the world of other main class");
+System.out.println("date is 24th december 2017");
+
+int i=99;
+if(i==99) {
+System.out.println("it works correctly");
+System.out.println("another sys out message");
 
 }
 
