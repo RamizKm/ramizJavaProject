a98ff723712675a27da8c5670a6d7d235adbf534
RamizKm <mominramiz@gmail.com>
2018-06-05 13:31:12 +0530

    ramiz commit change

--- src/CalendarObj.java
+++ src/CalendarObj.java
@@ -5,7 +5,7 @@ public class CalendarObj {
 	
 	public static void main(String[] args) {
 		System.out.println("CalendarObj.main()");
-		
+		System.ou.println("testing for stash has been started.");
 		Calendar d=Calendar.getInstance();
 		System.out.println(d);
 		System.out.println(d.getTime().getTime());
